3e5f558226cf5ef5e88686f25855aa4c7984025a
Filippo Biondi <filibiondi2000@gmail.com>
2025-06-10 09:12:46 +0200

    renamed proxy

---
+++ api/proxy/process.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  const baseUrl = process.env.BACKEND_URL
+  const targetPath = req.url.replace(/^\/api\/proxy/, '')
+  const targetUrl = `${baseUrl}${targetPath}`
+  console.log(`Proxy: ${targetUrl}`)
+
+  const response = await fetch(targetUrl, {
+    method: req.method,
+    headers: {
+      'Content-Type': req.headers['content-type'] || 'application/json',
+    },
+    body:
+      req.method !== 'GET' && req.method !== 'HEAD'
+        ? req.body
+        : undefined,
+  })
+
+  const data = await response.text()
+  res.status(response.status).send(data)
+}
